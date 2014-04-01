4d901d5473fdea8702dd4c7977ddcbfaa6f191d3
ppj <prasanna_3003@yahoo.com>
2014-04-01 22:47:08 +1100

    spare scoring and a little more

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/invalid_roll_score_exception'
 class Frame
-  attr_accessor :score
+
   def initialize(rolls)
     if rolls.length > 2
       raise "frame cannot have more than 2 rolls"
@@ -8,7 +8,34 @@ class Frame
     rolls.each {|roll| raise InvalidRollScoreException.new("roll score is invalid") if roll < 0 or roll > 10}
     @rolls = rolls
     @score = rolls.inject(0){|sum,pins| sum+pins}
+    @bonus = -1
   end
 
+  def score
+    @score = spare_score if spare?
+    @score
+  end
+
+
+  def spare?
+    @score == 10 and @rolls[0]<10
+  end
+
+  def strike?
+    @rolls[0] == 10
+  end
+
+  def bonus_score(next_rolls)
+    @bonus = next_rolls[0]
+  end
+
+  private
+
+  def spare_score
+    @score = '/'
+    @score = 10 + @bonus if @bonus > -1
+    @bonus = -1
+    @score
+  end
 
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -5,4 +5,39 @@ describe Frame do
     expect { Frame.new([-3,33])}.to raise_exception("roll score is invalid")
     expect { Frame.new([33,-3])}.to raise_exception("roll score is invalid")
   end
+
+  context 'calculates score' do
+    let(:subject) do
+      Frame.new([3,3])
+    end
+    it 'gives the score for the simple frame' do
+      expect(subject.score).to eq(6)
+    end
+    it 'gives the score for the spare' do
+      subject = Frame.new([5,5])
+      subject.bonus_score([2])
+      expect(subject.score).to eq(12)
+    end
+  end
+  context 'for a spare' do
+    it 'cannot give the score without bonus' do
+      subject = Frame.new([5,5])
+      expect(subject.score).to eq('/')
+    end
+
+  end
+  context 'knows about spare or strike' do
+    it 'knows if its a spare' do
+      subject = Frame.new([5,5])
+      expect(subject.spare?).to be true
+      subject = Frame.new([0,10])
+      expect((Frame.new([0,10])).spare?).to be true
+
+    end
+    it 'knows if its a strike' do
+      subject = Frame.new([10,0])
+      expect(subject.strike?).to be true
+      expect(subject.spare?).to be false
+    end
+  end
 end
